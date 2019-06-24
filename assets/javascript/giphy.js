dcbe4c3bcaaff3a9f55588931daed18df5f1de4c
Soheila Lotfi <49765334+Soheila-Lotfi@users.noreply.github.com>
2019-06-24 13:01:07 -0400

    Update giphy.js

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,7 +2,7 @@
 $("button").click(function(){
     var person=$(this).attr("data-person");   // get the data-person of this (the button that is clicked by the user)
 
-    var queryUrl= "http://api.giphy.com/v1/gifs/search?q="+person+"&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=3";
+    var queryUrl= "https://api.giphy.com/v1/gifs/search?q="+person+"&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=3";
 
     $.ajax({
         url: queryUrl,
